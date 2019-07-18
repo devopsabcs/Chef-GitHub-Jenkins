20cd351a5d21694c2936841cb36daaaeb648ac1e
torben.verdich <torben_jm@verdich.org>
2019-07-18 16:02:13 +0000

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+package 'apache2' do
+	   action :install
+end
+file '/var/www/html/index.html' do
+	   action :create
+	    
+	  content 'Hello Pipeline World!'
+	        end
+service 'apache2' do
+	   action [ :enable, :start ]
+end
---
+++ test/integration/default/default_test.rb
@@ -0,0 +1,18 @@
+# InSpec test for recipe apache::default
+
+# The InSpec reference, with examples and extensive documentation, can be
+# found at https://www.inspec.io/docs/reference/resources/
+
+# This is an example test, replace it with your own test.
+describe package('apache2') do
+   it { should be_installed }
+end
+describe file('/var/www/html/index.html') do
+	        it { should exist }
+		  its('content') { should match(/Hello Pipeline World!/) } 
+		  end
+		  describe upstart_service('apache2') do
+		     it { should be_enabled }
+		        it { should be_running }
+			end
+
