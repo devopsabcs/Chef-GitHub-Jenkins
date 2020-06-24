724db02bf15ca732ede3819e407edc5b104d4569
Emmanuel Knafo <emknafo@microsoft.com>
2020-06-24 19:05:13 -0400

    1st end 2 end example!

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@ end
 file '/var/www/html/index.html' do
 	   action :create
 	    
-	  content "<h2>This is #{node['name']}</h2><h1>Hello World</h1>"
+	  content "<h2>This is #{node['name']}</h2><h1>Hello World End to end!</h1>"
 	        end
 service 'apache2' do
 	   action [ :enable, :start ]
